663d3afe8db0a5242cafef7319dbad98ee6677ed
Ariel Luiz <ariel.luiz.ferreira.s@gmail.com>
2026-04-13 13:35:17 -0300

    FEAT: Add README

--- TeacherControl/DTOs/Requests/LessonRequestDto.cs
+++ TeacherControl/DTOs/Requests/LessonRequestDto.cs
@@ -7,5 +7,5 @@ public class LessonRequestDto
     public DateOnly Date { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Guid UserId { get; set; }
+    public Guid UserId { get; set; } 
 }
